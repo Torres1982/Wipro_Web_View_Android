c94d34970125ed9d9b8f992ab5ce9076fc587240
Artur Torres <artur.sukiennik82@gmail.com>
2019-05-01 14:49:54 +0100

    [1] Layout updated with css. [2] Passing data from JS to native Android. [3] Alert Dialog Builder added for confirmation messages.

--- app/src/main/assets/src/js/main.js
+++ app/src/main/assets/src/js/main.js
@@ -8,73 +8,20 @@ function validateFields() {
 	var gender_selected = gender.val();
 
 	if (first_name.length < 2) {
-		alert("Provide your First Name, please!");
+	    Android.validate("Provide your First Name, please!");
 	} else if (surname.length < 2) {
-		alert("Provide your Surname, please!");
+		Android.validate("Provide your Surname, please!");
 	}  else if (!validateEmail(email)) {
-		alert("Provide Correct Format Email, please!");
+		Android.validate("Provide Correct Format Email, please!");
 	} else if (gender.length === 0) {
-		alert("Select Gender, please!");
+		Android.validate("Select Gender, please!");
 	} else if (dob == "") {
-    	alert("Select Date of Birth, please!");
+    	Android.validate("Select Date of Birth, please!");
   	} else {
-		confirm("Are you sure you want to submit your data?");
-
-	    //displayJsonArray(first_name, surname, email, gender_selected, dob);
-	    //saveDataToLocalStorage(first_name, surname, email, gender_selected, dob);
+  	    var message = "Are you sure you want to submit your data?";
+		Android.confirmSubmission(message, first_name, surname, email, dob, gender_selected);
 	}
-	Android.validate(first_name);
 }
-
-// This function returns a String of the user details
-//function displayJsonArray (first_name, surname, email, gender, dob) {
-//    var user_details = '{"new_user":[{"first_name": ' + first_name + ', "last_name": ' + surname + ', "email": ' + email + ', "gender": ' + gender +', "date_of_birth": ' + dob + '}]}';
-//    console.log(user_details);
-//}
-
-// Saves the registered user in the local storage
-//function saveDataToLocalStorage(first_name, last_name, email, gender, dob) {
-//    addUserDataToLocalDatabase("first_name", first_name);
-//    addUserDataToLocalDatabase("last_name", last_name);
-//    addUserDataToLocalDatabase("email", email);
-//    addUserDataToLocalDatabase("gender", gender);
-//    addUserDataToLocalDatabase("date_of_birth", dob);
-//}
-
-//function addUserDataToLocalDatabase(key, value) {
-//	localStorage.setItem(key, value);
-//}
-
-// Retrieves the registered user from the local storage
-//$('.nav_users').click(function() {
-//    retrieveDataFromLocalStorage();
-//});
-
-//function retrieveDataFromLocalStorage() {
-//    var first_name = localStorage.getItem("first_name");
-//    var surname = localStorage.getItem("last_name");
-//    var email = localStorage.getItem("email");
-//    var gender = localStorage.getItem("gender");
-//    var dob = localStorage.getItem("date_of_birth");
-//    var user_array = [first_name, surname, email, gender, dob];
-//
-//    populateInputFields(user_array);
-//}
-
-// Populate the input fields with data from the local storage
-//function populateInputFields(user_array) {
-//    $("#first-name").val(user_array[0]);
-//    $("#surname").val(user_array[1]);
-//    $("#email").val(user_array[2]);
-//    $("#date-of-birth").val(user_array[4]);
-//	var gender = user_array[3] === "female" ? "female" : "male";
-//
-//	checkGenderCheckBox(gender);
-//}
-
-//function checkGenderCheckBox(gender) {
-//	$('#'+gender).prop("checked", true);
-//}
 
 // Simple email validation
 function validateEmail(email) {
--- app/src/main/java/com/wipro/wiprowebview/JavaScriptReceiver.java
+++ app/src/main/java/com/wipro/wiprowebview/JavaScriptReceiver.java
@@ -1,6 +1,8 @@
 package com.wipro.wiprowebview;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -12,10 +14,39 @@ public class JavaScriptReceiver {
     }
 
     @JavascriptInterface
-    public void validate(String name) {
-        Toast.makeText(context, "!!! Testing Web View !!! " + name, Toast.LENGTH_SHORT).show();
-//        Intent intent = new Intent(context, MainActivity.class);
-//        context.startActivity(intent);
+    public void validate(String validationString) {
+        Toast.makeText(context, validationString, Toast.LENGTH_SHORT).show();
+    }
 
+    @JavascriptInterface
+    //message, first_name, surname, email, dob, gender_selected
+    public void confirmSubmission(String message, String name, String surname, String email, String dob, String gender) {
+        showConfirmationDialog(message, name, surname, email, dob, gender);
+    }
+
+    // Create a Confirmation Alert Dialog
+    private void showConfirmationDialog(String message, String name, String surname, String email, String dob, String gender) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.confirmation_dialog_title);
+        builder.setMessage(message + "\nName:\t\t\t\t" + name + " " + surname + "\nEmail:\t\t\t\t" + email + "\nDOB:\t\t\t\t\t" + dob + "\nGender:\t\t" + gender);
+
+        builder.setPositiveButton(R.string.dialog_box_positive_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(context, "Data has been submitted successfully! " + which, Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(R.string.dialog_box_negative_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(context, "Data submission has been cancelled! " + which, Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
